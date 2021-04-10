c0f83ce3c9602d653fb2c054f2e63cf08d8e4b57
pavelklos <pavelkloscz@gmail.com>
2021-04-10 11:19:59 +0200

    188. [11:42] Rendering Dynamic Post Pages & Paths

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -3,15 +3,24 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  // console.log(post);
 
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown className={classes.md}>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown className={classes.md}>{post.content}</ReactMarkdown>
     </article>
   );
+  // const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  // return (
+  //   <article className={classes.content}>
+  //     <PostHeader title={DUMMY_POST.title} image={imagePath} />
+  //     <ReactMarkdown className={classes.md}>{DUMMY_POST.content}</ReactMarkdown>
+  //   </article>
+  // );
 }
 
 export default PostContent;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,39 @@
 // rfce
 import PostContent from "../../components/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  // console.log(props.post);
+
+  // return <PostContent />;
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  // console.log(slug);
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600, // 600 sec = 10 min
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  // console.log(slugs);
+
+  return {
+    // paths: [{ params: { slug: "getting-started-with-nextjs" } }],
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false, // true, 'blocking'
+  };
 }
 
 export default PostDetailPage;
