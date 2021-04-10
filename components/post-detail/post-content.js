7b24761e64ccad9ba97d8c0e65f9665f6c311250
pavelklos <pavelkloscz@gmail.com>
2021-04-10 13:23:56 +0200

    189. [12:46] Rendering Images With The "Next Image" Component (From Markdown)

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,5 +1,6 @@
 // rfce
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 
@@ -8,10 +9,45 @@ function PostContent(props) {
   // console.log(post);
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      // if image is in paragraph [ERROR : ISSUE]
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown className={classes.md}>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers} className={classes.md}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
   // const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
